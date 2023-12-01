40681f7fe0bf0f71587f54c681a2334cc59c8a8b
Biner Habib <Binerhabib@gmail.com>
2023-12-01 12:36:36 +0100

    Add project files.

---
+++ pokemon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emne3_KlasserOgProperties
+{
+    internal class pokemon
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Level { get; set; }
+
+        public pokemon(string name, int health, int level)
+        {
+            Name = name;
+            Health = health;
+            Level = level;
+        }
+
+        public void showInfo()
+        {
+           console.WriteLine($)
+        }
+    }
+}
